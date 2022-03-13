f297722fd1ee601de635b0ca7030b600e36904bc
Elian Valdez <elian.valdez09@gmail.com>
2022-03-13 16:40:59 -0300

    a

--- src/index.js
+++ src/index.js
@@ -13,29 +13,29 @@ const io = require("socket.io")(server);
 
 app.use(cookieParser());
 app.use(helmet());
+app.use(cors())
+// var allowedOrigins = [
+//   "http://localhost:3000",
+//   "https://portal-tricordiano.vercel.app/",
+// ];
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       // allow requests with no origin
+//       // (like mobile apps or curl requests)
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         var msg =
+//           "The CORS policy for this site does not " +
+//           "allow access from the specified Origin.";
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
-var allowedOrigins = [
-  "http://localhost:3000",
-  "https://portal-tricordiano.vercel.app/",
-];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
-
-// app.use(morgan("dev"));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(
   express.urlencoded({
